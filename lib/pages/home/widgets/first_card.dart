f62dc16aeff7d308bd72de201fac9e0b14aa781c
Victor Oliveira <victoroliveira.160595@gmail.com>
2020-04-20 18:35:50 -0300

    First Card

--- lib/pages/home/widgets/first_card.dart
+++ lib/pages/home/widgets/first_card.dart
@@ -14,16 +14,122 @@ class FirstCard extends StatelessWidget {
                   children: <Widget>[
                     Expanded(
                       child: Column(
-                        children: <Widget>[],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 20, right: 20, top: 30),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.credit_card,
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(
+                                  width: 15,
+                                ),
+                                Text(
+                                  'Cartão de crédito',
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 16),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 20, right: 20, top: 60),
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  'FATURA ATUAL',
+                                  style: TextStyle(
+                                      color: Colors.cyan,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 20, right: 20, top: 5),
+                            child: Row(
+                              children: <Widget>[
+                                Text.rich(
+                                  TextSpan(
+                                    text: 'R\$ ',
+                                    children: [
+                                      TextSpan(
+                                        text: '600',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      TextSpan(text: ',54'),
+                                    ],
+                                    style: TextStyle(
+                                        color: Colors.cyan, fontSize: 28),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 20, right: 20, top: 5),
+                            child: Row(
+                              children: <Widget>[
+                                Text.rich(
+                                  TextSpan(
+                                    text: 'Limite disponível ',
+                                    style: TextStyle(
+                                        color: Colors.black87, fontSize: 14),
+                                    children: [
+                                      TextSpan(
+                                        text: 'R\$ 23,33',
+                                        style: TextStyle(
+                                            color: Colors.green,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(bottom: 12, left: 10),
-                      child: Container(
-                        width: 8,
-                        decoration: BoxDecoration(
-                            color: Colors.orange,
-                            borderRadius: BorderRadius.circular(2)),
+                      padding: const EdgeInsets.only(left: 10),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(2),
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                flex: 2,
+                                child: Container(
+                                  color: Colors.orange,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 8,
+                                child: Container(
+                                  color: Colors.lightBlue,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Container(color: Colors.lightGreen),
+                              )
+                            ],
+                          ),
+                          width: 8,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(2)),
+                        ),
                       ),
                     ),
                   ],
@@ -33,6 +139,28 @@ class FirstCard extends StatelessWidget {
             flex: 1,
             child: Container(
               color: Colors.grey[200],
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.shopping_cart,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(
+                      child: Text(
+                        'Compra mais recente em Super Mercado no valor de R\$ 30,05 sabado',
+                        style: TextStyle(color: Colors.grey, fontSize: 13),
+                      ),
+                    ),
+                    Icon(Icons.chevron_right,
+                        color: Colors.grey[400], size: 18),
+                  ],
+                ),
+              ),
             ),
           )
         ],
